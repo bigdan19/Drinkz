3c54823e6dbd900ff92db034e132caa350f4c72b
Daniel <daniel@Daniels-Mac-Pro.local>
2023-02-17 21:24:02 +0000

    added basic api call to provide list of popular drinks

--- Drinkz/HomeVC.swift
+++ Drinkz/HomeVC.swift
@@ -6,12 +6,48 @@
 //
 
 import UIKit
+import SDWebImage
 
 class HomeVC: UIViewController {
     
     var images: [UIImage] = []
     
+    var popularDrinks = [Drink]()
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewWillLayoutSubviews() {
+        
+        let urlString = "https://www.thecocktaildb.com/api/json/v2/9973533/popular.php"
+        
+        
+        if let url = URL(string: urlString) {
+            
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    self.parse(json: data)
+                    DispatchQueue.main.async {
+                        self.collectionView.reloadData()
+                    }
+                } else {
+                    print("HTTP Request failed")
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonDrinks = try? decoder.decode(listOfPopularDrinks.self, from: json) {
+            popularDrinks = jsonDrinks.drinks
+        } else {
+            print("Error occured decoding data")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,18 +58,22 @@ class HomeVC: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        
+        
     }
 }
 
+
 extension HomeVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        8
+        popularDrinks.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "popularCoctailsCell", for: indexPath) as! PopularCoctailsCollectionViewCell
-        cell.image.image = images.randomElement()
-        cell.label.text = "Whiskey Sour"
+        let url = URL(string: popularDrinks[indexPath.item].strDrinkThumb)
+        cell.image.sd_setImage(with: url)
+        cell.label.text = popularDrinks[indexPath.item].strDrink
         return cell
     }
 }
